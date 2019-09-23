c56c42a028a0c5b346ec5999529f951dbb27e721
Jacob Lifshay <programmerjake@gmail.com>
2019-09-23 16:29:16 -0600

    add Dockerfile and flesh out somewhat

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[cfg(feature = "python")]
+use pyo3::prelude::*;
+
+#[cfg(feature = "python-extension")]
+#[pymodule]
+fn rust_python_test(py: Python, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m, "my_function")]
+    fn my_function(_py: Python, i: String) -> PyResult<String> {
+        Ok(i + " suffix")
     }
+    Ok(())
 }
